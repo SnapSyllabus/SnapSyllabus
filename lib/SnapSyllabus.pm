567bf69618a6f71340fb755dab141de1b35aa64e
Snap Syllabus <snapsyllabus@ip-10-254-54-87.us-west-2.compute.internal>
2013-04-21 00:51:11 +0000

    misc

--- lib/SnapSyllabus.pm
+++ lib/SnapSyllabus.pm
@@ -1,10 +1,24 @@
 package SnapSyllabus;
 use Dancer ':syntax';
 
-our $VERSION = '0.1';
+set 'session'      => 'Simple';
+set 'template'      => 'template_toolkit';
+set 'layout'      => 'main';
 
 get '/' => sub {
-    template 'index';
+    template 'index', {}, { layout => undef };
 };
 
 true;
+
+=head1 NAME
+
+   SnapSyllabus - 3DS product
+
+=head1 DESCRIPTION
+
+A webapp developed at 3DS University of Houston.
+
+=cut
+
+# ABSTRACT: turns a syllabus into a calendar
